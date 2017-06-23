90cd836cc45f9e6b46f57614df2bbb34a03631f3
oliver <oliver.hock@videa-services.com>
2017-06-23 15:54:34 +0200

    initialise project including first test example

---
+++ videa-bpm-examples-testcoverage/videa-bpm-examples-testcoverage/src/main/java/services/videa/bpm/examples/testcoverage/App.java
@@ -0,0 +1,13 @@
+package services.videa.bpm.examples.testcoverage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
