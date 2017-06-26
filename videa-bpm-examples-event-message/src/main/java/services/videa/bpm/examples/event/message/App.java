9767f8a34f79078a8bb8b160c496d076fd920e16
oliver <oliver.hock@videa-services.com>
2017-06-26 13:44:41 +0200

    initially add project

---
+++ videa-bpm-examples-event-message/src/main/java/services/videa/bpm/examples/event/message/App.java
@@ -0,0 +1,13 @@
+package services.videa.bpm.examples.event.message;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
